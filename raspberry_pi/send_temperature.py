44884db369eb18617626df638a62191e20322db8
Bob Berger <bob@bobberger.me>
2016-12-19 20:23:04 -0500

    send_temperature.py reads configuration from applicaiton.cfg

--- raspberry_pi/send_temperature.py
+++ raspberry_pi/send_temperature.py
@@ -2,35 +2,38 @@
 
 # Send temperature readings to a MQTT topic
 
+import os
 import time
 import json
 from datetime import datetime
+import ConfigParser
 import paho.mqtt.client as mqtt
 from ds18b20 import DS18B20
 
-# TODO move this to a config fie
-device_locations = { '28-000003cb57a4': 'ny/outdoor',  '28-0000044b383b': 'ny/den' }
-mqtt_host = '127.0.0.1'
-
 delay = 30.0
 
-device_ids = device_locations.keys()
+dir = os.path.dirname(os.path.realpath(__file__))
+config = ConfigParser.SafeConfigParser()
+config.read(dir + '/application.cfg')
+mqtt_broker = config.get('mqtt','broker')
+
+sensor_locations = {}
 sensors = {}
-for id in device_ids:
+for id, location in config.items('sensors'):
+    sensor_locations[id] = location
     sensors[id] = DS18B20(id)
 
+
 mqtt_client = mqtt.Client()
-mqtt_client.connect(mqtt_host)
+mqtt_client.connect(mqtt_broker)
 mqtt_client.loop_start()
 
 while True:
-    for id in device_ids:
-        location = device_locations[id]
+    for id in sensor_locations.keys():
+        location = sensor_locations[id]
         topic =  "sensor/temperature/" + location
         temperature = sensors[id].read_fahrenheit()
         msg = json.dumps({ 'location' : location, 'datetime':  str(datetime.now()), 'type': 'temperature', 'value': temperature })
         mqtt_client.publish(topic, msg)
 
     time.sleep(delay)
-
-
